5f2b988c065843b3c42548af42beb2b85b84dfbe
Nagi <84936494+nagiqui@users.noreply.github.com>
2025-11-18 22:53:13 +0100

    Bump version to 1.03 and enhance metadata
    Updated the script metadata for better clarity and added English descriptions.

--- animeclick-skip-video-intro/Blocca-AnimeClick-video-intro.user.js
+++ animeclick-skip-video-intro/Blocca-AnimeClick-video-intro.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Blocca AnimeClick video-intro
+// @name:en      Block AnimeClick video-intro
 // @namespace    Skip-Animeclick-Video-Intro
-// @version      1.02
+// @version      1.03
 // @description  Salta automaticamente la pagina "video-intro" di AnimeClick.it portando direttamente alla homepage.
+// @description:en This script automatically skips the “video-intro” page on AnimeClick.it, an Italian Anime and Manga website, and redirects you directly to the homepage.
 // @match        https://www.animeclick.it/*
 // @run-at       document-start
 // @grant        none
@@ -16,6 +18,7 @@
         location.replace('https://www.animeclick.it/');
     }
 })();
+
 
 
 
